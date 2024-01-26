cca0fd04b7c85fe26d5801b86bb28c854c79919f
John Doe <alabi.charles24@gmail.com>
2024-01-26 09:00:18 +0000

    trying react

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {AiFillPhone, AiOutlineClockCircle} from 'react-icons/ai'
+import {BsChatSquareDots} from 'react-icons/bs'
+
+const TopBar = () => {
+    return (
+        <div>
+            <div>
+                <BsChatSquareDots />
+                <h1>StudioIn</h1>
+            </div>
+        </div>
+    )
+}
+export default TopBar
